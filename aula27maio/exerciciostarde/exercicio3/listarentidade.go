278e1e6f63b923747c8c4808ad83d943035a9511
Marta Denisczwicz Rozsa <marta.denisczwicz@gmail.com>
2022-05-31 08:03:44 -0300

    Update exercise

--- aula27maio/exerciciostarde/exercicio3/listarentidade.go
+++ aula27maio/exerciciostarde/exercicio3/listarentidade.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
 /*
 Exercício 3 - Listar Entidade
 Já tendo criado e testado nossa API que nos recebe, geramos uma rota que retorna uma lista do tema escolhido.
@@ -8,3 +13,31 @@ Já tendo criado e testado nossa API que nos recebe, geramos uma rota que retorn
 3. Crie uma handler para o endpoint chamada "GetAll".
 4. Crie um slice da estrutura e retorne-a por meio de nosso endpoint.
 */
+
+type produto struct {
+	id          int64   `json:"id"`
+	nome        string  `json:"nome"`
+	cor         string  `json:"tipo"`
+	preco       float64 `json:"quantidade"`
+	estoque     bool    `json:"preco"`
+	codigo      string  `json:"codigo"`
+	publicacao  bool    `json:"publicacao"`
+	dataCriacao string  `json:"dataCriacao"`
+}
+
+type produtos []produto
+
+var prod produto
+
+func listProdutos(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"Produtos": &prod})
+}
+
+func main() {
+
+	r := gin.Default()
+
+	r.GET("/produtos", listProdutos)
+
+	r.Run()
+}
